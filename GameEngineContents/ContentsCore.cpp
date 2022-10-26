59e6cf19c666ed94b9a6baf039e391d475b52e89
Eletos <lthlovelyg@naver.com>
2022-10-26 13:14:06 +0900

    이관형 : 컨텐츠 정리

--- GameEngineContents/ContentsCore.cpp
+++ GameEngineContents/ContentsCore.cpp
@@ -1,12 +1,8 @@
 #include "PreCompile.h"
 #include "ContentsCore.h"
-#include "LoginLevel.h"
-#include "PlayLevel.h"
-#include "TestLevel.h"
-#include "MapEditorLevel.h"
-#include "MetaLevel.h"
-
-//#pragma comment(lib, "GameEngineBase.lib")
+#include "TitleLevel.h"
+#include "WorldMapLevel.h"
+#include "StageLevel.h"
 
 ContentsCore::ContentsCore()
 {
@@ -18,49 +14,26 @@ ContentsCore::~ContentsCore()
 
 void ContentsCore::Start()
 {
-
-
-	GameEngineDirectory contentsResourceDir;
-	contentsResourceDir.MoveParentToExistChildDirectory("ContentsResources");
-	contentsResourceDir.MoveToChild("ContentsResources");
-	contentsResourceDir.MoveToChild("Texture");
-
-	std::vector<GameEngineFile> contentsTextures = contentsResourceDir.GetAllFiles();
-
-	for (size_t i = 0; i < contentsTextures.size(); i++)
-	{
-		GameEngineTexture::Load(contentsTextures[i].GetFullPath());
-	}
-
 	GameEngineFont::Load("궁서");
-
-
 
 	if (false == GameEngineInput::GetInst()->IsKey("LevelChangeKey"))
 	{
 		GameEngineInput::GetInst()->CreateKey("LevelChangeKey", 'P');
 	}
 
-
 	//GameEngineDebug::ConsoleOpen(); 콘솔창이 필요하면 복원.
 
 	//RTTI(Run-Time Type information): 런타임 형식 정보. 
 	//프로그램 실행 중간에 데이터 타입을 받아오는 방법. 
 
+	CreateLevel<TitleLevel>("Title");
+	CreateLevel<WorldMapLevel>("WorldMap");
+	CreateLevel<StageLevel>("Stage");
+
+	ChangeLevel("Stage");
 
 
-	CreateLevel<LoginLevel>("Login");
-	CreateLevel<PlayLevel>("Play");
-	CreateLevel<TestLevel>("TestLevel");
-	CreateLevel<MapEditorLevel>("MapEditor");
-	CreateLevel<MetaLevel>("MetaEditor");
-
-	ChangeLevel("Login");
-
-
-	GameEngineTime::SetFrameLimit(60);
-
-
+	//GameEngineTime::SetFrameLimit(60);
 	GameEngineGUI::CreateGUIWindow<GameEngineStatusWindow>("EngineStatus", nullptr);
 }
 
