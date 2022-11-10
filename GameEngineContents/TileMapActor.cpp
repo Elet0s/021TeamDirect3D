f74a8d8fc532d222a82180216cfc1a6a42776614
우진 안 <kagepro@naver.com>
2022-11-10 16:26:54 +0900

    안우진: 1110 전투스테이지 풀 오브젝트 업데이트

--- GameEngineContents/TileMapActor.cpp
+++ GameEngineContents/TileMapActor.cpp
@@ -16,17 +16,27 @@ void TileMapActor::Start()
 {
 	tileRenderer_ = CreateComponent<MyTileMapRenderer>();
 
-	/*for (size_t i = 0; i < 100; i++)
+	for (size_t i = 0; i < 80; i++)
 	{
-		float X = static_cast<float>(GameEngineRandom::mainRandom_.RandomInt(0, 80));
-		float Y = static_cast<float>(GameEngineRandom::mainRandom_.RandomInt(0, 80));
+		float X = static_cast<float>(GameEngineRandom::mainRandom_.RandomInt(0, 40));
+		float Y = static_cast<float>(GameEngineRandom::mainRandom_.RandomInt(1, 42));
 		X *= 32.0f;
-		Y *= 32.0f;
-		GameEngineTextureRenderer* TextureRenderer = CreateComponent<GameEngineTextureRenderer>();
-		TextureRenderer->SetFolderTextureToIndex("Grass", GameEngineRandom::mainRandom_.RandomInt(0,2));
-		TextureRenderer->ScaleToTexture();
-		TextureRenderer->GetTransform().SetWorldPosition(float4{ X,-Y, -500.f});
-	}*/
+		Y *= 16.0f;
+		if (i < 70)
+		{
+			GameEngineTextureRenderer* TextureRenderer = CreateComponent<GameEngineTextureRenderer>();
+			TextureRenderer->SetFolderTextureToIndex("Grass", GameEngineRandom::mainRandom_.RandomInt(0, 2));
+			TextureRenderer->ScaleToTexture();
+			TextureRenderer->GetTransform().SetWorldPosition(float4{ X,-Y, -500.f });
+		}
+		else if (i > 75)
+		{
+			GameEngineTextureRenderer* TextureRenderer = CreateComponent<GameEngineTextureRenderer>();
+			TextureRenderer->SetFolderTextureToIndex("Grass", 3);
+			TextureRenderer->ScaleToTexture();
+			TextureRenderer->GetTransform().SetWorldPosition(float4{ X,-Y, -500.f });
+		}
+	}
 
 
 
