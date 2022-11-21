254f2e99470a0c797d137d6f518a869ae862e4a4
Eletos <lthlovelyg@naver.com>
2022-11-21 20:36:06 +0900

    이관형 : ptr문제

--- GameEngineContents/PlayerUI.cpp
+++ GameEngineContents/PlayerUI.cpp
@@ -14,37 +14,26 @@ PlayerUI::~PlayerUI()
 }
 void PlayerUI::Start()
 {
-
-
-
 	GameEngineDirectory Dir;
 	Dir.MoveParentToExistChildDirectory("ContentsResources");
 	Dir.MoveToChild("ContentsResources");
 	Dir.MoveToChild("UI");
 	Dir.MoveToChild("PlayerUI");
 
-	if (Player::GetPlayerWeakPtr().expired())
-	{
-		int i = 0;
-	}
-
 	player_ = Player::GetPlayerWeakPtr();
-
+	this->SetParent(player_.lock()->CastThis<Player>());
 	playerHpUi_ = CreateComponent<GameEngineTextureRenderer>();
-	playerHpUi_->GetTransform().SetWorldScale(100, 100, 1);
-	playerHpUi_->GetTransform().SetWorldPosition(
-		player_.lock()->GetTransform().GetWorldPosition().x,
-		player_.lock()->GetTransform().GetWorldPosition().y + 50.0f, 
-		-100
-	);
+	playerHpUi_->GetTransform().SetWorldScale(100, 10, 1);
 	playerHpUi_->SetTexture("DefaultUi.png");
-
-
-
+	//playerHpUi_->GetPixelData().mulColor_ = float4(1.0f,0.0f,0.0f,1.0f);	//ºÓÀº»ö
+	//playerHpUi_->GetPixelData().mulColor_ = float4(0.0f,1.0f,0.0f,1.0f);	//³ì»ö
+	//playerHpUi_->GetPixelData().mulColor_ = float4(1.0f,1.0f,1.0f,1.0f); //Èò»ö
+	//playerHpUi_->GetPixelData().mulColor_ = float4(0.0f, 0.0f, 0.0f, 1.0f); //°ËÀº»ö
+	playerHpUi_->GetPixelData().slice_ = float4(0.5f,0.0f,0.0f,0.0f);
 }
 void PlayerUI::Update(float _deltaTime)
 {
-
+	playerHpUi_->GetTransform().SetWorldPosition(player_.lock()->GetTransform().GetWorldPosition().x, player_.lock()->GetTransform().GetWorldPosition().y + 100.0f, -100);
 }
 void PlayerUI::End()
 {
