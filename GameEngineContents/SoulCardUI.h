eacf787cd69826fe4e3beba8d84ea58f4ab24679
우진 안 <kagepro@naver.com>
2022-11-29 16:31:08 +0900

    안우진: 스킬 선택 UI 선택 카드 구현

--- GameEngineContents/SoulCardUI.h
+++ GameEngineContents/SoulCardUI.h
@@ -17,8 +17,14 @@ public:
 protected:
 
 private:
+	std::shared_ptr<GameEngineTextureRenderer> linerenderer_;
 	std::shared_ptr<GameEngineTextureRenderer> template_;
 	std::shared_ptr<GameEngineTextureRenderer> icon_;
+	std::vector<std::shared_ptr<GameEngineFontRenderer>> etc_;
+	std::shared_ptr<GameEngineFontRenderer> skillName_;
+	std::shared_ptr<GameEngineFontRenderer> Level_;
+	std::shared_ptr<GameEngineFontRenderer> Rank_;
+	std::shared_ptr<GameEngineCollision> cardColision_;
 
 	void Start() override;
 	void Update(float _deltaTime);
