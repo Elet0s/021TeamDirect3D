65e8d98273d68633d5526410265145c54170165c
Jaein2022 <dlwodls10@naver.com>
2022-12-12 21:55:16 +0900

    이재인 : 1212 테스트레벨에 몬스터 숫자 증가.

--- GameEngineContents/TestLevel.cpp
+++ GameEngineContents/TestLevel.cpp
@@ -128,20 +128,26 @@ void TestLevel::Start()
 	{
 		std::shared_ptr<PlayerUI> NewPlayerUI = CreateActor<PlayerUI>(ObjectOrder::UI);
 	}
-	Monster::ReserveMonsters(this, 100);
-	
-	Monster::CreateMonster<BlackEyes>(this, 1);
-	Monster::SummonMonster<BlackEyes>(this, 1);
+
+	//Monster::ReserveMonsters(this, 100);
+
+	//Monster::CreateMonster<BlackEyes>(this, 1);
+	//Monster::SummonMonster<BlackEyes>(this, 1);
 	
 
-	//Monster::CreateMonster<Brown>(this, 10);
-	//Monster::CreateMonster<Green>(this, 10);
-	//Monster::CreateMonster<NormalGoblin>(this, 10);
-	//Monster::CreateMonster<RedFlyingEyes>(this, 10);
-	//Monster::CreateMonster<FlyingEyes>(this, 10);
-	//
-	//Monster::SummonMonster<FlyingEyes>(this, 8);
-	//Monster::SummonMonster<FlyingEyes>(this, 11);
+	Monster::ReserveMonsters(this, 50);
+
+	Monster::CreateMonster<Brown>(this, 10);
+	Monster::CreateMonster<Green>(this, 10);
+	Monster::CreateMonster<NormalGoblin>(this, 10);
+	Monster::CreateMonster<RedFlyingEyes>(this, 10);
+	Monster::CreateMonster<FlyingEyes>(this, 10);
+
+	Monster::SummonMonster<Brown>(this, 10);
+	Monster::SummonMonster<Green>(this, 10);
+	Monster::SummonMonster<NormalGoblin>(this, 10);
+	Monster::SummonMonster<RedFlyingEyes>(this, 10);
+	Monster::SummonMonster<FlyingEyes>(this, 10);
 
 	
 	std::shared_ptr<Mouse> NewMouse = CreateActor<Mouse>(ObjectOrder::Mouse);
