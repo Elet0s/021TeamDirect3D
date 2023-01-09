2c0850998c6af78228807d51aa0314cd6edaee72
Eletos <lthlovelyg@naver.com>
2023-01-09 16:08:29 +0900

    이관형 : 스킬랜더 카메라 변경

--- GameEngineContents/DeathAura.cpp
+++ GameEngineContents/DeathAura.cpp
@@ -92,31 +92,41 @@ void DeathAura::Start()
 	circleRendererA_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererA_->SetTexture("Death_Circle_A.png");
 	circleRendererA_->GetTransform().SetWorldScale(515, 515, 0);
-	circleRendererA_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.7f);
+	circleRendererA_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.5f);
+	circleRendererA_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererA_->Off();
 
 	circleRendererB_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererB_->SetTexture("Death_Circle_B.png");
 	circleRendererB_->GetTransform().SetWorldScale(500, 500, 0);
 	circleRendererB_->GetTransform().SetWorldPosition(0.f, 0.f, -1.f);
-	circleRendererB_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.7f);
+	circleRendererB_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.5f);
+	circleRendererB_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererB_->Off();
 
 	circleRendererC_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererC_->SetTexture("Death_Circle_C.png");
 	circleRendererC_->GetTransform().SetWorldScale(475, 475, 0);
 	circleRendererC_->GetTransform().SetWorldPosition(0.f, 0.f, -2.f);
-	circleRendererC_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.7f);
+	circleRendererC_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.5f);
+	circleRendererC_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererC_->Off();
 
 	circleRendererD_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererD_->SetTexture("Death_Circle_D.png");
 	circleRendererD_->GetTransform().SetWorldScale(475, 475, 0);
 	circleRendererD_->GetTransform().SetWorldPosition(0.f, 0.f, -3.f);
-	circleRendererD_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.7f);
+	circleRendererD_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.5f);
+	circleRendererD_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererD_->Off();
 
 	circleCenterRenderer_ = CreateComponent<GameEngineTextureRenderer>();
 	circleCenterRenderer_->SetTexture("Death_Circle_Center.png");
 	circleCenterRenderer_->GetTransform().SetWorldScale(475, 475, 0);
 	circleCenterRenderer_->GetTransform().SetWorldPosition(0.f, 0.f, -4.f);
-	circleCenterRenderer_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.7f);
+	circleCenterRenderer_->GetPixelData().mulColor_ = float4(0.0f, 1.5f, 0.0f, 0.5f);
+	circleCenterRenderer_->ChangeCamera(CameraOrder::MidCamera);
+	circleCenterRenderer_->Off();
 
 	deathAuraCollision01_ = CreateComponent<GameEngineCollision>();
 	deathAuraCollision01_->SetDebugSetting(CollisionType::CT_Sphere, float4::Blue);
@@ -171,6 +181,14 @@ void DeathAura::RotateRenderer(float _deltaTime)
 	circleRendererC_->GetTransform().SetWorldRotation(60, 0, addRadian_);
 	circleRendererD_->GetTransform().SetWorldRotation(60, 0, -addRadian_);
 	circleCenterRenderer_->GetTransform().SetWorldRotation(60, 0, addRadian_);
+	if (circleRendererA_->IsUpdate() == false)
+	{
+		circleRendererA_->On();
+		circleRendererB_->On();
+		circleRendererC_->On();
+		circleRendererD_->On();
+		circleCenterRenderer_->On();
+	}
 }
 void DeathAura::End()
 {
--- GameEngineContents/Firering.cpp
+++ GameEngineContents/Firering.cpp
@@ -30,30 +30,40 @@ void Firering::Start()
 	circleRendererA_->SetTexture("Cast_Circle_A.png");
 	circleRendererA_->GetTransform().SetWorldScale(515, 515, 0);
 	circleRendererA_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.0f);
+	circleRendererA_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererA_->Off();
 
 	circleRendererB_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererB_->SetTexture("Cast_Circle_B.png");
 	circleRendererB_->GetTransform().SetWorldScale(500, 500, 0);
 	circleRendererB_->GetTransform().SetWorldPosition(0.f, 0.f, -1.f);
 	circleRendererB_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.0f);
+	circleRendererB_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererB_->Off();
 
 	circleRendererC_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererC_->SetTexture("Cast_Circle_C.png");
 	circleRendererC_->GetTransform().SetWorldScale(200, 200, 0);
 	circleRendererC_->GetTransform().SetWorldPosition(0.f, 0.f, -2.f);
 	circleRendererC_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.0f);
+	circleRendererC_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererC_->Off();
 
 	circleRendererD_ = CreateComponent<GameEngineTextureRenderer>();
 	circleRendererD_->SetTexture("Cast_Circle_D.png");
 	circleRendererD_->GetTransform().SetWorldScale(200, 200, 0);
 	circleRendererD_->GetTransform().SetWorldPosition(0.f, 0.f, -3.f);
 	circleRendererD_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.0f);
+	circleRendererD_->ChangeCamera(CameraOrder::MidCamera);
+	circleRendererD_->Off();
 
 	circleCenterRenderer_ = CreateComponent<GameEngineTextureRenderer>();
 	circleCenterRenderer_->SetTexture("Cast_Circle_E.png");
 	circleCenterRenderer_->GetTransform().SetWorldScale(200, 200, 0);
 	circleCenterRenderer_->GetTransform().SetWorldPosition(0.f, 0.f, -4.f);
 	circleCenterRenderer_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.0f);
+	circleCenterRenderer_->ChangeCamera(CameraOrder::MidCamera);
+	circleCenterRenderer_->Off();
 
 	fireringAuraCollision01_ = CreateComponent<GameEngineCollision>();
 	fireringAuraCollision01_->SetDebugSetting(CollisionType::CT_Sphere, float4::Blue);
@@ -118,13 +128,40 @@ void Firering::Update(float _deltaTime)
 	fireringAuraCollision03_->GetTransform().SetWorldPosition(fireringAuraCollision01_->GetTransform().GetWorldPosition().x - 100.f, fireringAuraCollision01_->GetTransform().GetWorldPosition().y, 0.f);
 	atkTimer_ += _deltaTime;
 
-	if (atkTimer_ > fireringAuraWeaponInfo_.weaponAtkSpeed_)
+
+
+
+
+		if  (atkTimer_ < fireringAuraWeaponInfo_.weaponAtkSpeed_ / 2)
+		{
+			circleRendererA_->GetPixelData().mulColor_ -= float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererB_->GetPixelData().mulColor_ -= float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererC_->GetPixelData().mulColor_ -= float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererD_->GetPixelData().mulColor_ -= float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleCenterRenderer_->GetPixelData().mulColor_ -= float4(0.0f, 0.0f, 0.0f, 0.1f);
+		}
+		else if (atkTimer_ > fireringAuraWeaponInfo_.weaponAtkSpeed_ / 2)
+		{
+			circleRendererA_->GetPixelData().mulColor_ += float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererB_->GetPixelData().mulColor_ += float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererC_->GetPixelData().mulColor_ += float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleRendererD_->GetPixelData().mulColor_ += float4(0.0f, 0.0f, 0.0f, 0.1f);
+			circleCenterRenderer_->GetPixelData().mulColor_ += float4(0.0f, 0.0f, 0.0f, 0.1f);
+		}
+
+	 if (atkTimer_ > fireringAuraWeaponInfo_.weaponAtkSpeed_)
 	{
+		circleRendererA_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.f);
+		circleRendererB_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.f);
+		circleRendererC_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.f);
+		circleRendererD_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.f);
+		circleCenterRenderer_->GetPixelData().mulColor_ = float4(1.0f, 1.0f, 0.0f, 1.f);
 		atkTimer_ = 0;
 		fireringAuraCollision01_->IsCollision(CollisionType::CT_Sphere2D, ObjectOrder::Monster, CollisionType::CT_Sphere2D, std::bind(&Firering::ProjectileToMonsterCollision, this, std::placeholders::_1, std::placeholders::_2));
 		fireringAuraCollision02_->IsCollision(CollisionType::CT_Sphere2D, ObjectOrder::Monster, CollisionType::CT_Sphere2D, std::bind(&Firering::ProjectileToMonsterCollision, this, std::placeholders::_1, std::placeholders::_2));
 		fireringAuraCollision03_->IsCollision(CollisionType::CT_Sphere2D, ObjectOrder::Monster, CollisionType::CT_Sphere2D, std::bind(&Firering::ProjectileToMonsterCollision, this, std::placeholders::_1, std::placeholders::_2));
 	}
+
 }
 void Firering::End()
 {
@@ -140,12 +177,21 @@ void Firering::RotateRenderer(float _deltaTime)
 	{
 		addRadian_ = 0;
 	}
-
 	circleRendererA_->GetTransform().SetWorldRotation(60, 0, addRadian_);
 	circleRendererB_->GetTransform().SetWorldRotation(60, 0, -addRadian_);
 	circleRendererC_->GetTransform().SetWorldRotation(60, 0, addRadian_);
 	circleRendererD_->GetTransform().SetWorldRotation(60, 0, -addRadian_);
 	circleCenterRenderer_->GetTransform().SetWorldRotation(60, 0, addRadian_);
+	if (circleRendererA_->IsUpdate() ==false)
+	{
+		circleRendererA_->On();
+		circleRendererB_->On();
+		circleRendererC_->On();
+		circleRendererD_->On();
+		circleCenterRenderer_->On();
+	}
+
+
 }
 
 CollisionReturn Firering::ProjectileToMonsterCollision(std::shared_ptr<GameEngineCollision> _This, std::shared_ptr<GameEngineCollision> _Other)
